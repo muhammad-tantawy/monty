981d55d13ab9436d40af09d1f5e7a3241a81c473
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-18 14:22:40 +0300

    adding_code

--- main.c
+++ main.c
@@ -4,12 +4,34 @@
 
 int main(int ac , char **av)
 {
-	stack_t *mem;
-	mem = NULL;
+	stack_t *stack;
+	stack = NULL;
 	if (ac!= 2)
 	{
 		fprintf(stderr, "USAGE: monty file \n");
 		exit(EXIT_FAILURE);
 	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
 
