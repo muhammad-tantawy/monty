4615f748476a33a0e0e620d98b1b7072c50d96be
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-18 03:19:15 +0300

    adding_code

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac , char **av)
+{
+	stack_t *mem;
+	mem = NULL;
+	if (ac!= 2)
+	{
+		fprintf(stderr, "USAGE: monty file \n");
+		exit(EXIT_FAILURE);
+	}
+}
+
